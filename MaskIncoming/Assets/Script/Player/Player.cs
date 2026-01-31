2ad6cf30b13852ad8828f20e2a2eb9b9d03fc8bc
porterSigma <davide.zamprogn02@gmail.com>
2026-01-31 17:12:40 +0100

    add cinemachine camera

--- MaskIncoming/Assets/Script/Player/Player.cs
+++ MaskIncoming/Assets/Script/Player/Player.cs
@@ -7,14 +7,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private Animator animator;
+    private Rigidbody2D m_Rb;
+    private Vector2 m_MoveInput;
+    private Animator m_Animator;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        m_Rb = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -29,27 +29,27 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        m_Rb.linearVelocity = m_MoveInput * moveSpeed;
     }
 
     public void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
+        m_MoveInput = value.Get<Vector2>();
 
-        if (moveInput != Vector2.zero)
+        if (m_MoveInput != Vector2.zero)
         {
-            animator.SetFloat("MoveX", moveInput.x);
-            animator.SetFloat("MoveY", moveInput.y);
+            m_Animator.SetFloat("MoveX", m_MoveInput.x);
+            m_Animator.SetFloat("MoveY", m_MoveInput.y);
         }
 
-        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+        m_Animator.SetFloat("Speed", m_MoveInput.sqrMagnitude);
     }
 
     public void OnMaskPower(InputValue value)
     {
         if (value.isPressed)
         {
-            animator.SetTrigger("SpecialPower");
+            m_Animator.SetTrigger("SpecialPower");
             Debug.Log("Potere Speciale!");
         }
     }
