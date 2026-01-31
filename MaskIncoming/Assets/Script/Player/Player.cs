bc31245f37df8230da8c7b251947d5d93759060e
porterSigma <davide.zamprogn02@gmail.com>
2026-01-31 15:58:21 +0100

    add character animation and input

--- MaskIncoming/Assets/Script/Player/Player.cs
+++ MaskIncoming/Assets/Script/Player/Player.cs
@@ -1,16 +1,80 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+    private PlayerControls controls;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        controls = new PlayerControls();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void OnEnable() => controls.Player.Enable();
+    void OnDisable() => controls.Player.Disable();
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
     void Update()
     {
-        
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+
+        if (moveInput != Vector2.zero)
+        {
+            animator.SetFloat("MoveX", moveInput.x);
+            animator.SetFloat("MoveY", moveInput.y);
+                    Debug.Log("Potere Speciale!");
+        }
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if (controls.Player.MaskPower.triggered)
+        {
+            ActivatePower();
+        }
+    }
+
+    // public void OnMove(InputValue value)
+    // {
+    //     moveInput = value.Get<Vector2>();
+
+    //     if (moveInput != Vector2.zero)
+    //     {
+    //         animator.SetFloat("MoveX", moveInput.x);
+    //         animator.SetFloat("MoveY", moveInput.y);
+    //         Debug.Log("Move!");
+    //     }
+
+    //     animator.SetFloat("Speed", moveInput.sqrMagnitude);
+    // }
+
+    // public void OnMaskPower(InputValue value)
+    // {
+    //     if (value.isPressed)
+    //     {
+    //         animator.SetTrigger("SpecialPower");
+    //         Debug.Log("Potere Speciale!");
+    //     }
+    // }
+
+    void ActivatePower()
+    {
+        Debug.Log("Mask Power Activate");
     }
 }
