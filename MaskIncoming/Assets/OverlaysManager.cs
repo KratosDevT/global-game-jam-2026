2bd1ac4ce6a4fe01ce84cbb2f81759103aa784eb
KratosDevT <simone.trevisan.gamedev@gmail.com>
2026-02-01 16:07:22 +0100

    overlay fix

--- MaskIncoming/Assets/OverlaysManager.cs
+++ MaskIncoming/Assets/OverlaysManager.cs
@@ -1,15 +1,16 @@
+using UnityEditor;
 using UnityEngine;
 
 public class OverlaysManager : MonoBehaviour
 {
-    
-    void Start()
+    public void Exit()
     {
-        
-    }
 
-    void Update()
-    {
-        
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;  // Editor: ferma Play Mode
+#else
+            Application.Quit();  // Build: chiude app
+#endif
+
     }
 }
